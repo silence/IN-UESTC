452a0bb20be9a57d3939665b67b66c8d2455ae87
silence <silence.since2017@gmail.com>
2019-09-30 20:40:12 +0800

    feat(WIP): 课程表

---
+++ src/pages/date/__mock__data.js
@@ -0,0 +1,121 @@
+const testData = [
+  {
+    course_id: 1,
+    course_name: '日语语言文化入门',
+    course_time: '3-4',
+    course_week: '1-17',
+    course_day: 1,
+    course_location: '品学楼C-119',
+    course_teacher: '鞠婧祎'
+  },
+  {
+    course_id: 2,
+    course_name: '微积分II',
+    course_time: '5-6',
+    course_week: '1-17',
+    course_day: 1,
+    course_location: '品学楼A-308',
+    course_teacher: '鞠婧祎'
+  },
+  {
+    course_id: 3,
+    course_name: '电路分析基础',
+    course_time: '1-2',
+    course_week: '1-17',
+    course_day: 1,
+    course_location: '品学楼A-116',
+    course_teacher: '鞠婧祎'
+  },
+  {
+    course_id: 4,
+    course_name: '大学物理I',
+    course_time: '3-4',
+    course_week: '1-17',
+    course_day: 2,
+    course_location: '品学楼A-309',
+    course_teacher: '鞠婧祎'
+  },
+  {
+    course_id: 5,
+    course_name: '中国现代史纲要',
+    course_time: '5-6',
+    course_week: '1-17',
+    course_day: 2,
+    course_location: '立人楼B-103',
+    course_teacher: '鞠婧祎'
+  },
+  {
+    course_id: 6,
+    course_name: '电子技术实验基础I',
+    course_time: '9-10',
+    course_week: '1-17',
+    course_day: 2,
+    course_location: '科研2号楼427',
+    course_teacher: '鞠婧祎'
+  },
+  {
+    course_id: 2,
+    course_name: '微积分II',
+    course_time: '1-2',
+    course_week: '1-17',
+    course_day: 3,
+    course_location: '品学楼A308',
+    course_teacher: '鞠婧祎'
+  },
+  {
+    course_id: 3,
+    course_name: '电路分析基础',
+    course_time: '3-4',
+    course_week: '1-17',
+    course_day: 5,
+    course_location: '品学楼A-106',
+    course_teacher: '鞠婧祎'
+  },
+  {
+    course_id: 7,
+    course_name: '飞行原理概论',
+    course_time: '5-6',
+    course_week: '1-17',
+    course_day: 3,
+    course_location: '品学楼A-208',
+    course_teacher: '鞠婧祎'
+  },
+  {
+    course_id: 4,
+    course_name: '大学物理I',
+    course_time: '1-2',
+    course_week: '1-17',
+    course_day: 4,
+    course_location: '品学楼A-309',
+    course_teacher: '鞠婧祎'
+  },
+  {
+    course_id: 8,
+    course_name: '数据结构',
+    course_time: '3-4',
+    course_week: '1-17',
+    course_day: 4,
+    course_location: '品学楼C-404',
+    course_teacher: '鞠婧祎'
+  },
+  {
+    course_id: 9,
+    course_name: '大学体育II',
+    course_time: '5-6',
+    course_week: '1-17',
+    course_day: 4,
+    course_location: '',
+    course_teacher: '鞠婧祎'
+  },
+  {
+    course_id: 3,
+    course_name: '电路分析基础',
+    course_time: '3-4',
+    course_week: '1-17',
+    course_day: 5,
+    course_location: '品学楼A-106',
+    course_teacher: '鞠婧祎'
+  }
+]
+
+export default testData
--- src/pages/date/main.tsx
+++ src/pages/date/main.tsx
@@ -1,10 +1,41 @@
 import Taro from '@tarojs/taro'
 import { View, Text } from '@tarojs/components'
+import Header from '@/components/SimpleHeader'
+import './main.scss'
 
 export default () => {
   return (
-    <View>
-      <Text>date/main</Text>
+    
+<View className="date-page">
+  <Header isBack bg title="IN成电"></Header>
+  <View className="date-axis">
+    <View  
+      
+      
+      className={`date-axis-item ${weekday.curr ? 'currday' : ''}`}
+    >
+      <View className="day">{ weekday.day }</View>
+      <View className="date">{ weekday.date }</View>
     </View>
+  </View>
+  <View className="no-axis">
+    <View
+      className="no-axis-item"
+    
+    >
+      { val }
+    </View>
+  </View>
+  <View className="date-body">
+    <course 
+      wx:for="{{ testData }}"
+      wx:for-item="item"
+      wx:for-index="idx"
+      wx:key="idx"
+      course="{{ item }}" 
+      selectHandle="selectHandle" />
+  </View>
+</View>
+
   )
 }
