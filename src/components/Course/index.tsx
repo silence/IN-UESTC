e2fe30d7b6c4e68f4afd710b4075f868df776856
silence <silence.since2017@gmail.com>
2019-10-02 23:55:47 +0800

    fix: 修正课表页类名

--- src/components/Course/index.tsx
+++ src/components/Course/index.tsx
@@ -1,20 +1,24 @@
 import Taro from '@tarojs/taro'
-import { View, Text } from '@tarojs/components'
+import { View } from '@tarojs/components'
 import './index.scss'
 
 interface IProps {
   course: {
     course_name: string
-    color: any
-    course_location: any
+    color: string
+    course_location: string
   }
-  classList: any
+  classList: Array<string>
 }
 
 export default (props: IProps) => {
   const course = props.course
   return (
-    <View className={props.classList} onClick={() => {}} style={{ background: course.color }}>
+    <View
+      className={props.classList.join(' ')}
+      onClick={() => console.log(props)}
+      style={{ background: course.color }}
+    >
       <View className="course-name">{course.course_name}</View>
       <View className="course-location">{course.course_location}</View>
     </View>
