f09c06d45624f5d3f6b2c4321663dc9a171b668d
silence <silence.since2017@gmail.com>
2019-10-03 01:22:50 +0800

    fix: 添加课程组件

--- src/components/Course/index.tsx
+++ src/components/Course/index.tsx
@@ -1,21 +1,38 @@
-import Taro from '@tarojs/taro'
+import Taro, { useState, useEffect } from '@tarojs/taro'
 import { View } from '@tarojs/components'
 import './index.scss'
 
 interface IProps {
   course: {
-    course_name: string
     color: string
+    course_day: number
+    course_id: number
     course_location: string
+    course_name: string
+    course_teacher: string
+    course_time: string
+    course_week: string
   }
-  classList: Array<string>
 }
 
 export default (props: IProps) => {
+  const [classList, setClassList] = useState(new Array())
   const course = props.course
+  useEffect(() => {
+    const [course_start, course_end] = course.course_time.split('-').map(v => Number(v))
+    const courseLen = course_end - course_start
+    const courseDay = course.course_day
+    setClassList(() => [
+      'course',
+      `row-${courseLen + 1}`,
+      `top-${course_start}`,
+      `left-${courseDay}`
+    ])
+    // 设为空数组，只会执行一次
+  }, [])
   return (
     <View
-      className={props.classList.join(' ')}
+      className={classList.join(' ')}
       onClick={() => console.log(props)}
       style={{ background: course.color }}
     >
--- src/pages/date/main.tsx
+++ src/pages/date/main.tsx
@@ -1,32 +1,57 @@
 import Taro from '@tarojs/taro'
 import { View, Text } from '@tarojs/components'
 import Header from '@/components/SimpleHeader'
+import Course from '@/components/Course'
 import './main.scss'
 
 export default () => {
   return (
-    
-<View className="date-page">
-  <Header isBack bg title="IN成电"></Header>
-  <View className="date-axis">
-    <View  
-      
-      
-      className={`date-axis-item ${weekday.curr ? 'currday' : ''}`}
-    >
-      <View className="day">{ weekday.day }</View>
-      <View className="date">{ weekday.date }</View>
+    <View className="date-page">
+      <Header isBack bg title="IN成电"></Header>
+      <View className="date-axis">
+        <View className={`date-axis-item ${weekday.curr ? 'currday' : ''}`}>
+          <View className="day">{weekday.day}</View>
+          <View className="date">{weekday.date}</View>
+        </View>
+      </View>
+      <View className="no-axis">
+        <View className="no-axis-item">{val}</View>
+      </View>
+      <View className="date-body">
+        <Course course={{}} selectHandle="selectHandle" />
+      </View>
     </View>
-  </View>
-  <View className="no-axis">
-    <View
-      className="no-axis-item"
-    
+  )
+}
+
+/**
+ * <!--pages/date/main.wxml-->
+<view class="date-page">
+  <header isBack bg title="IN成电"></header>
+  <view class="date-axis">
+    <view  
+      wx:for="{{weeks}}" 
+      wx:for-index="idx" 
+      wx:key="idx" 
+      wx:for-item="weekday"
+      class="{{ ['date-axis-item', weekday.curr ? 'currday' : ''] }}"
     >
-      { val }
-    </View>
-  </View>
-  <View className="date-body">
+      <view class="day">{{ weekday.day }}</view>
+      <view class="date">{{ weekday.date }}</view>
+    </view>
+  </view>
+  <view class="no-axis">
+    <view
+      class="no-axis-item"
+      wx:for="{{noAxis}}"
+      wx:for-index="idx"
+      wx:for-item="val"
+      wx:key="idx"
+    >
+      {{ val }}
+    </view>
+  </view>
+  <view class="date-body">
     <course 
       wx:for="{{ testData }}"
       wx:for-item="item"
@@ -34,8 +59,7 @@ export default () => {
       wx:key="idx"
       course="{{ item }}" 
       selectHandle="selectHandle" />
-  </View>
-</View>
+  </view>
+</view>
 
-  )
-}
+ */
